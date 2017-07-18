2cd7d3f4988803ec97d4e01fceb0db2736601df7
Elizabeth Evans <ickabeth@gmail.com>
2017-07-18 11:24:31 -0400

    accordiant locations styling and functionality

--- base.js
+++ base.js
@@ -1,9 +1,16 @@
 
+$(document).ready(function(e){
+
+// LOCATIONS expand function START
+  $('.item').click(function (e){
+    if($(this).next('.item-data').css('display') != 'block'){
+      $('.active').slideUp('fast').removeClass('active');
+      $(this).next('.item-data').addClass('active').slideDown('slow');
+    } else {
+      $('.active').slideUp('slow').removeClass('active');
+    }
+  });
+// LOCATIONS expand function END
 
 
-
-$(document).ready(function(){
-    $(".indicator").click(function(){
-        $(".item-data").toggleClass("closed");
-    });
 });
